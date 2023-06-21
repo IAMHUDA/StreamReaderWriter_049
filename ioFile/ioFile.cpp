243165a7077ad36f6dd71a46008ad280b7000124
IAMHUDA <hudamiftahul362@gmail.com>
2023-06-21 14:48:57 +0700

    menambahkan of stream untuk membuka file dengan mode menulis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -5,5 +5,14 @@
 using namespace std;
 
 int main() {
+	string baris;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file 
+	outfile.open("contohfile.txt");
+
+	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
 
 } 
